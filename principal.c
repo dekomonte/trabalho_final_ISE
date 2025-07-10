bb836fa23074110952338c9d1f8df09305bbd983
Andressa Monteiro <dekomonte@yahoo.com.br>
2025-07-09 22:32:34 -0300

    Atualizando arquivo principal

--- principal.c
+++ principal.c
@@ -22,6 +22,9 @@
 #define DEVICES_HEX_DISPLAY_02 (0x00000030)
 #define DEVICES_SWITCHES (0x00000040)
 #define DEVICES_BUTTONS  (0x00000050)
+#define HEX_DISPLAY1_BASE ((volatile uint32_t *)(LW_BRIDGE_BASE + DEVICES_HEX_DISPLAY_01))
+#define HEX_DISPLAY2_BASE ((volatile uint32_t *)(LW_BRIDGE_BASE + DEVICES_HEX_DISPLAY_02))
+
 
 //Cores
 #define BLUE 0x001F
@@ -36,7 +39,10 @@
 #define FOREST_GREEN 0x0280
 #define DARK_GREEN 0x03E0
 
-
+volatile uint32_t *leds;
+volatile unsigned int *switches, *buttons;
+volatile uint32_t *hex_display1, *hex_display2;
+	
 //Array de pixels
 uint16_t *ppix = (uint16_t *)(VGA_BASE);
 
@@ -44,6 +50,16 @@ void set_pixel(int linha, int coluna){
 	if((linha >= 0 && linha < 240) && (coluna >= 0 && coluna <320)){
 		ppix[linha * 512 + coluna] = DARK_GREEN;
 	}
+}
+
+//Funcao de delay
+void delay(unsigned int tempo_delay){
+    volatile unsigned int i, j;
+    for (i = 0; i < tempo_delay; i++) {
+        for (j = 0; j < 10000; j++) {
+            // Só para gastar tempo
+        }
+    }
 }
 
 //Seta a cor de fundo do VGA 
@@ -55,9 +71,70 @@ void muda_cor_fundo(){
     }
 }
 
+//Funcoes que escrevem no display de 7 segmentos
+void write_hex_menu(const char display[]) {
+    *(hex_display1) = display[3] << 24 |display[2]<< 16 | display[1] << 8 | display[0];
+	*(hex_display2) = display[5] << 8 | display[4];
+}
+
+void write_hex(int value[]) {
+    char display[] = {0x3F, 0x3F, 0x3F, 0x3F, 0x3F, 0x3F};
+    int i = 5;
+    int pos = 0;
+
+    while (i >= 0) {
+        switch (value[i]) {
+            case 0: display[pos] = 0x3F; break;
+            case 1: display[pos] = 0x06; break;
+            case 2: display[pos] = 0x5B; break;
+            case 3: display[pos] = 0x4F; break;
+            case 4: display[pos] = 0x66; break;
+            case 5: display[pos] = 0x6D; break;
+            case 6: display[pos] = 0x7D; break;
+            case 7: display[pos] = 0x07; break;
+            case 8: display[pos] = 0x7F; break;
+            case 9: display[pos] = 0x6F; break;
+            default: display[pos] = 0x3F; break;
+        }
+        i--; 
+        pos++;
+    }
+
+    *(hex_display1) = display[3] << 24 | display[2] << 16 | display[1] << 8 | display[0];
+    *(hex_display2) = display[5] << 8 | display[4];
+}
+
+//Mostra os pontos do jogador no display de 7 segmentos
+void mostra_pontos(int pontos){
+	
+	int digitos[6] = {0}; //Limita o total de pontos para 6 digitos
+	
+	if (pontos < 0){
+		pontos = 0;
+	}
+	
+	if (pontos > 999999){
+		pontos = 999999;
+	}
+	
+	for(int i = 5; i >= 0; i--){
+		digitos[i] = pontos % 10;
+		pontos /= 10;
+	}
+	
+	write_hex(digitos);
+	
+}
 
 int main(){
 
+	hex_display1 = HEX_DISPLAY1_BASE;
+	hex_display2 = HEX_DISPLAY2_BASE;
+
+	int pontos = 543647;
+	
+	mostra_pontos(pontos);
+	
 	muda_cor_fundo();	
 	//while(1){
 		
